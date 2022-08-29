b53ab07715adbd3285f045a29c7a4f3e59c08def
flavynn <flavenhow18@gmail.com>
2022-08-29 19:44:32 +0200

    Exercicio 1 e 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const actual = species.find((specie) => specie.name === animal);
+  const { residents } = actual;
+  const checkResidents = residents.filter((resident) => resident.age >= age);
+  if (checkResidents.length === residents.length) return true;
+  return false;
 }
 
 module.exports = getAnimalsOlderThan;
+
+// Implemente a função getAnimalsOlderThan que deve receber uma espécie e uma idade como parâmetro, e então retornar se todos os animais dessa espécie  possuem essa idade ou são mais velhos. X
+
+// Verifique se todos os animais da espécie passada como parâmetro possuem a idade mínima: X
+
+// Os animais devem ter essa idade ou serem mais velhos.X
+// Retorne um valor booleano. X
+
+// O que será testado:
+
+// A função, ao receber uma espécie e uma idade como parâmetros, deve testar se todos os animais desta espécie possuem a idade mínima especificada. X
