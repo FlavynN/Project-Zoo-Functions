40dc1d5a045771bf10674a6f26dfd5f0fd1c6f8c
flavynn <flavenhow18@gmail.com>
2022-09-01 23:40:44 +0200

    Exercicio 5 Feito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // digite seu codigo
+  if (!animal) {
+    const totalAnimais = species.reduce((acc, animais) => {
+      acc[animais.name] = animais.residents.length;
+      return acc;
+    }, {});
+    return totalAnimais;
+  }
+  if (!animal.sex) {
+    return species.find((animales) => animales.name === animal.specie).residents.length;
+  }
+  if (animal.specie && animal.sex) {
+    return species.find((animales) => animales.name === animal.specie).residents
+      .filter((animales) => animales.sex === animal.sex).length;
+  }
 }
 
 module.exports = countAnimals;
