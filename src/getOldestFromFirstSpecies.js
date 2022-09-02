772fed5e731d92f93c16739fc357dc605999f8fd
flavynn <flavenhow18@gmail.com>
2022-09-02 06:54:09 +0200

    Exercicio 10 feito

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const colaborador = employees.find((colab) => colab.id === id);
+  const maisVelho = species.find((specie) => specie.id === colaborador.responsibleFor[0]).residents
+    .sort((animalA, animalB) => animalB.age - animalA.age)[0];
+  return Object.values(maisVelho);
 }
 
 module.exports = getOldestFromFirstSpecies;
